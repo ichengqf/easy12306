4852e9f176eaabd3b012519d8598788174e3a5fb
yifuda <xin_soft@foxmail.com>
2018-11-18 22:25:38 +0800

    把训练集放置在单个文件中。

--- DownloadImg.py
+++ DownloadImg.py
@@ -19,12 +19,12 @@ def download_img(idx):
     url = 'https://kyfw.12306.cn/otn/passcodeNew/getPassCodeNew?module=login&rand=sjrand'
     response = requests.get(url)
     fn = hashlib.md5(response.content).hexdigest()
-    with open(f'img/{fn}.jpg', 'wb') as fp:
+    with open(f'imgs/{fn}.jpg', 'wb') as fp:
         fp.write(response.content)
 
 
 if __name__ == '__main__':
-    utils.mkdir('img')
+    utils.mkdir('imgs')
     # for idx in range(1000):
     #     download_img(idx)
     greenpool.spawn(download_img, range(1000))
--- pretreatment.py
+++ pretreatment.py
@@ -3,52 +3,31 @@
 # 功能：对图像进行预处理，将文字部分单独提取出来
 # 并存放到ocr目录下
 # 文件名为原验证码文件的文件名
-import cv2
 import os
 
-import utils
+import cv2
+import numpy as np
 
 
-def read_img(fn):
-    '''
-    得到验证码完整图像
-    :param fn:图像文件路径
-    :return:图像对象
-    '''
-    return cv2.imread(fn)
+def pretreat(path):
+    imgs = []
+    for img in os.listdir(path):
+        img = os.path.join(path, img)
+        img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+        # 得到图像中的文本部分
+        img = img[3:22, 120:177]
+        imgs.append(img)
+    return imgs
 
 
-def write_img(im, fn):
-    cv2.imwrite(fn, im)
-
-
-def get_text_img(im):
-    '''
-    得到图像中的文本部分
-    '''
-    return im[3:22, 127:184]
-
-
-def binarize(im):
-    '''
-    二值化图像
-    '''
-    gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
-    (retval, dst) = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
-    return dst
-
-
-def show_img(im):
-    print(im.ndim, im.dtype)
-    cv2.imshow("image", im)
-    cv2.waitKey(0)
+def load_data(path='data.npy'):
+    if not os.path.isfile(path):
+        imgs = pretreat('imgs')
+        np.save(path, imgs)
+    return np.load(path)
 
 
 if __name__ == '__main__':
-    utils.mkdir('ocr')
-    for img_name in os.listdir('img'):
-        im = read_img(os.path.join('img', img_name))
-        im = get_text_img(im)
-        im = binarize(im)
-        # show_img(im)
-        write_img(im, os.path.join('ocr', img_name))
+    imgs = load_data()
+    print(imgs.shape)
+    cv2.imwrite('temp.jpg', imgs[0])
